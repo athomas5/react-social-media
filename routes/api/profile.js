1a204542cb13a9652b90c9225a9cdafca0ded426
Andre Thomas <athomas@Andres-Mac-mini.local>
2018-06-22 16:49:13 -0400

    Implement delete profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -162,6 +162,47 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
     });
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    profile.experience = profile.experience.filter(experienceObj => {
+      return experienceObj._id.toString() !== req.params.exp_id;
+    });
+
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err));
+});
+
+// @route   DELETE api/profile/education/:exp_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      profile.education = profile.education.filter(educationObj => {
+        return educationObj._id.toString() !== req.params.edu_id;
+      });
+
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOneAndRemove({ user: req.user.id })
+    .then(() => {
+      User.findOneAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: 'User has been deleted' }));
+    }).catch(err => res.json(err));
+});
+
 const getProfileFields = (req) => {
   let profileFields = {};
   profileFields.user = req.user.id;
