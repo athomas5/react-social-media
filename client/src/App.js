3141052588615560527c14c663cd179db8f66576
André Thomas <athomas3@vt.edu>
2018-06-29 01:43:27 -0400

    Add Register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Login from './components/auth/Login/Login';
+import Register from './components/auth/Register/Register';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        {/* <Login /> */}
+        <Register />
       </div>
     );
   }
---
+++ client/src/components/auth/Register/Register.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Register.css';
+
+import Input from '../Input/Input';
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userEmail: '',
+      userPassword: ''
+    }
+  }
+
+  handleOnChange(e) {
+    if (e.target.type === 'email') {
+      this.setState({ userEmail: e.target.value });
+    }
+
+    if (e.target.type === 'password') {
+      this.setState({ userPassword: e.target.value });
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.userEmail,
+      password: this.state.userPassword
+    }
+  }
+
+  render() {
+    return (
+      <section className="login-container">
+        <h1 className="header-title">Social Media App</h1>
+        <form
+          action="POST"
+          className="login-form"
+          onSubmit={e => this.onSubmit(e)}>
+
+          <Input
+            type='email'
+            class='input input-email'
+            placeholder='Email address'
+            onChange={e => this.handleOnChange(e)} />
+
+          <Input
+            type='password'
+            class='input input-password'
+            placeholder='Password'
+            onChange={e => this.handleOnChange(e)} />
+
+          <button className="button-register" type="submit">Login</button>
+        </form>
+      </section>
+    )
+  }
+}
