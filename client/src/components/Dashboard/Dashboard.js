8be3351e9027f5b6052e804a8f7fc7dfe24b83fd
André Thomas <athomas3@vt.edu>
2018-07-18 01:51:46 -0400

    Set up dashboard content

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
@@ -8,21 +9,37 @@ class Dashboard extends Component {
     this.props.getCurrentProfile();
   }
 
-  render() {
+  getNoProfileMessage() {
     const { user } = this.props.auth;
-    const { profile, loading } = this.props.profile;
-
-    let dashboardContent;
-
-    if (profile === null || loading) {
-      dashboardContent = <h4>Loading...</h4>;
-    } else {
-      dashboardContent = <h1>Dashboard content</h1>;
-    }
 
     return (
+      <div className="text-container">
+        <h2 className="welcome-message">Welcome {user.name}</h2>
+        <p className="description">You haven't setup your profile yet</p>
+        <Link to="/create-profile" className="create-profile">Create Profile</Link>
+      </div>
+    );
+  }
+
+  getDashboardContent() {
+    const { profile, loading } = this.props.profile;
+
+    if (profile === null || loading) {
+      return <h4>Loading...</h4>;
+    } else {
+      if (Object.keys(profile).length > 0) {
+        return <h4>TODO: Display Profile</h4>
+      } else {
+        return this.getNoProfileMessage();
+      }
+    }
+  }
+
+  render() {
+    return (
       <section className="dashboard-container">
-        {dashboardContent}
+        <h1 className="title">Dashboard</h1>
+        {this.getDashboardContent()}
       </section>
     )
   }
